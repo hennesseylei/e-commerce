6645da6a197439cd1cf7d186b03fae62e88e0677
hennesseylei <hennesseylei@gmail.com>
2024-11-19 13:11:24 -0600

    flask!

--- server.py
+++ server.py
@@ -1,2 +1,14 @@
 print("dis the server")
 print('gnome bacon')
+
+
+from http import server
+from flask import Flask
+server = Flask(__name__)
+
+@server.route("/")
+def home():
+    return "Hello, Flask!"
+
+#flask --app server run (this ran the flask environment)
+
