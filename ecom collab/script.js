4c90e0fa223242b9dba4b70798492b025a182883
Brianna Townsend <breabout644@gmail.com>
2024-11-20 13:23:58 -0600

    Page overlay finally works :D

--- ecom collab/script.js
+++ ecom collab/script.js
@@ -1,9 +1,19 @@
-window.addEventListener("load", () => {
-    const overlay = document.querySelector(".pageoverlay");
-    const content = document.querySelector(".landingpage");
+function fadeOut() {
+    var fadeTarget1 = document.querySelector(".pageoverlay");
+    var fadeTarget2 = document.querySelector(".landingpage");
+    var fadeEffect = setInterval(function() {
+        if (!fadeTarget1.style.opacity > 0 && !fadeTarget2.style.opacity) {
+            fadeTarget1.style.opacity = 1;
+            fadeTarget2.style.opacity = 1;
+        }
+        if (fadeTarget1.style.opacity > 0 && fadeTarget2.style.opacity) {
+            fadeTarget1.style.opacity -= 0.1;
+            fadeTarget2.style.opacity -= 0.1;
+        } else {
+            clearInterval(fadeEffect);
+        }
+    }, 200);
+}
 
-    setTimeout(() => {
-        overlay.style.transform = "0";
-        content.style.opacity = "1";
-    }, 2000);
-});
+document.querySelector(".pageoverlay").addEventListener("click", fadeOut);
+document.querySelector(".landingpage"),addEventListener("click", fadeOut);
